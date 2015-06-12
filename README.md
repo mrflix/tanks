8ef76f1a81de146def285c454d1bc74bb4ea05dd
Felix Niklas <mrflix@gmail.com>
2015-06-12 14:31:08 +0200

    add authors to readme, add idea

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # tanks
-A tank game for the CCCB Airport Display
+A tank game for the CCCB Airport Display.
 
 ![Tank Game](/picture.jpg?raw=true)
+
+By Felix and [Steven](https://github.com/increpare).
--- server.js
+++ server.js
@@ -6,6 +6,9 @@
 
   Ideas:
       - Make the tank slower when you shoot (Denis)
+      - Stable Sort the Scoreboard so that the first person
+        getting the highscore stays on top until someone
+        scores higher
 
 */
 var tileSize = 8
